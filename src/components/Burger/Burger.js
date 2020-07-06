519e1d5f9df557b59b5e254bb8927fa644787772
Ghost-in-the-shell <shubm17@gmail.com>
2020-07-06 19:16:04 +0530

    added burger controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,25 @@ import './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
+
+    const modifiedIngredients = Object.keys(props.ingredients).map(
+        igKey => {
+            return [...Array(props.ingredients[igKey])].map(
+                (_,i)=>{
+                    return <BurgerIngredient key={igKey+i} type={igKey} />
+                }
+            )
+        }
+    ).reduce((prev,curr)=>{
+        return prev.concat(curr)
+    },[])
+
+console.log(modifiedIngredients)
     return (
+
         <div className="Burger">
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-
-            <BurgerIngredient type="meat" />
-
-            <BurgerIngredient type="salad" />
+            {modifiedIngredients}
             <BurgerIngredient type="bread-bottom" />
 
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            ingredients:{
+                salad:2,
+                bacon:1,
+                meat:1,
+                cheese:2
+            }
+        };
+    }
+    
+
     render() {
         return (
             <Aux>
-                <Burger />
-                <div>Burger-Controls</div>
+                <Burger  ingredients={this.state.ingredients}/>
+                <BuildControls />
             </Aux>
         );
     }
